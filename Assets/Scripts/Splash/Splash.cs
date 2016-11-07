661b645a28f26367fc0882c16337ffe005d5caf0
Rodrigo Dias <rodrigo.pitta@gmail.com>
2016-11-07 10:58:09 +0000

    Scenes and new game

---
+++ Assets/Scripts/Splash/Splash.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement; 
+
+public class Splash : MonoBehaviour {
+
+	void Update(){
+		StartCoroutine (W ());
+	}
+
+	IEnumerator W(){
+		yield return new WaitForSeconds (5);
+		SceneManager.LoadScene("MMenu");
+	}
+}
