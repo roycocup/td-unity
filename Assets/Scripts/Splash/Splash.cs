5b6af03b870e87e4da9f673025e80c2c92746fc9
vgd_rod <rodrigo@visiongroup-ltd.co.uk>
2016-11-10 18:31:59 +0000

    tried to get the logo rotated, but not happening

--- Assets/Scripts/Splash/Splash.cs
+++ Assets/Scripts/Splash/Splash.cs
@@ -4,12 +4,31 @@ using UnityEngine.SceneManagement;
 
 public class Splash : MonoBehaviour {
 
-	void Update(){
-		StartCoroutine (W ());
+	float timeToWait = 1.5f;
+	float timeWaited = 0;
+	int timeToChangeScene = 0;
+	GameObject logo; 
+
+
+	void Start () {
+		logo = GameObject.Find("/Canvas/Panel/Logo");
+		timeToChangeScene = 2 + (int) timeToWait;
 	}
 
-	IEnumerator W(){
-		yield return new WaitForSeconds (5);
-		SceneManager.LoadScene("MMenu");
+
+	void Update () {
+
+		// animation time
+		if (timeToWait < timeWaited) {
+			Quaternion rotation =  Quaternion.Lerp(transform.rotation, Quaternion.Euler(Vector3.forward * 90f), 1f);
+			transform.rotation = rotation;
+			if (timeToChangeScene < timeWaited ){
+				SceneManager.LoadScene("MMenu");
+			}
+		}
+
+		timeWaited += Time.deltaTime;
+
 	}
+
 }
