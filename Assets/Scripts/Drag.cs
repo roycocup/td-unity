14621590c95eb98107494ff194dcc27dbda6ecfd
vgd_rod <rodrigo@visiongroup-ltd.co.uk>
2016-11-24 16:57:22 +0000

    starting drag script

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Drag : MonoBehaviour {
 
+	float maxLeft = 12f;
+	float maxRight = 12f;
 
 	void Start () {
 	
@@ -11,23 +13,34 @@ public class Drag : MonoBehaviour {
 
 	void FixedUpdate () {
 
+		// mobile handling
 		if (Input.touchCount > 0) 
 		{
 			Touch touch = Input.GetTouch(0);
-
 			Ray ray = Camera.main.ScreenPointToRay(touch.position);
 
 			if (touch.phase == TouchPhase.Moved) 
 			{
-				MoveCamera (); 
+				//MoveCamera (); 
 			}        
 
 		}
+
+
 	}
 
 
 	void MoveCamera(){
-		
+		print ("Camera movement");
+	}
+
+	void OnMouseDrag() {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit[] hits = Physics.RaycastAll (ray);
+
+		if (hits [0].collider.name == "beach") {
+
+		}
 	}
 
 
