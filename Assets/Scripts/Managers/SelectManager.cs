f11816fadcd5cf8b5f5c3ecab832664ef716a24d
Rodrigo Dias <rodrigo.pitta@gmail.com>
2016-12-11 18:50:33 +0000

    small changes to selection

--- Assets/Scripts/Managers/SelectManager.cs
+++ Assets/Scripts/Managers/SelectManager.cs
@@ -6,6 +6,7 @@ using System;
 public class SelectManager : MonoBehaviour {
 
 	public Material _red;
+	public Material _white;
 
 	GameObject _go;
 	GameObject _previousObject; 
@@ -13,17 +14,32 @@ public class SelectManager : MonoBehaviour {
 	int _layerMask = 8;
 	bool _selected = false;
 
+	struct MaterialRegistry{
+		public Material material; 
+	}
+
 	void FixedUpdate () {
+
+		// look for things under the mouse
 		_go = getHoveringObject ();
+
+		// this may be a tower or a towerblock
+		// so it needs to be handled differently
 		if (_go != null) {
+			// set the record for the previous object
 			_previousObject = _go;
-			Select (_go);
+			if (_go.tag == "Tower") {
+				//TODO: do somehting with towers
+			} else {
+				// if this is clickable and its not a tower
+				Highlight (_go);
+			}
+
 		} else {
 			if (_previousObject != null) {
-				UnselectPrevious ();
+				RemoveHighlight ();
 			}
 		}
-
 	}
 
 
@@ -34,26 +50,27 @@ public class SelectManager : MonoBehaviour {
 		// TODO: only execute if UI is not enabled
 		if (hits.Length > 0){
 			foreach (RaycastHit h in hits) {
-				if (h.collider.gameObject.layer.Equals(8)){
-					return h.collider.gameObject;	
+				// if the object is in the clickable layer mask
+				if (h.collider.gameObject.layer.Equals(_layerMask)){
+					GameObject go = h.collider.gameObject; 
+					return go;
 				}
 			}
 		}
 		return null;
 	}
 
-	void Select(GameObject go){
+	void Highlight(GameObject go){
 		MeshRenderer mr = go.GetComponent<MeshRenderer> ();
-		_goOriginalMaterial = mr.material; 
+//		_matReg.material = mr.material;
 		mr.material = _red; 
 		_selected = true;
 	}
 
 
-	void UnselectPrevious(){
-		Debug.Log (_goOriginalMaterial.mainTexture.name);
+	void RemoveHighlight(){
 		MeshRenderer mr = _previousObject.GetComponent<MeshRenderer> ();
-		mr.material = _goOriginalMaterial; 
+		mr.material = _white; 
 		_previousObject = null;
 		_selected = false;
 	}
